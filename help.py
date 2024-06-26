4c614d47b204f9de197315b63f1c450ad2227760
Raul <168528192+Raulisr00t@users.noreply.github.com>
2024-06-26 15:58:56 +0400

    Create help.py

---
+++ help.py
@@ -0,0 +1,36 @@
+import sys
+import subprocess
+from colorama import Fore,Style
+
+try:
+    while True:
+        Fore.RED
+        user = input("Please enter a tool name: ")
+        Style.RESET_ALL
+        
+        tools = ["nmap","hydra","feroxbuster","enumlinux","wpscan","curl","ldapsearch","osintagram","sherlock","bettercap"]
+        try:
+            if user in tools:
+                if user.lower():
+                    print(f"{user} options\n")
+                    command = subprocess.Popen([f"{user}", "--help"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                    out, err = command.communicate()
+                    print(out.decode())
+                    print(err.decode())
+                
+                if not user:
+                    continue
+
+            elif user.lower() == "exit":
+                print("Program finished by user!\n")
+                sys.exit(0)
+                
+            else:
+                print("Try again!\n")
+
+        except Exception as e:
+            print("Your Error-->",e)
+            break
+
+except KeyboardInterrupt:
+    print("\nUser terminate the program!\n")
